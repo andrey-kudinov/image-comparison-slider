c2477ba1eb3c0546c67ec46c6dbe9e30beff67fa
Andrey Kudinov <64890665+AndreyKudinov63@users.noreply.github.com>
2021-10-27 21:41:24 +0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,52 @@
+import './App.css';
+
+import React, { useState } from 'react';
+
+import logo from './logo.svg';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p className="header">
+          🚀 Vite + React + Typescript 🤘 & <br />
+          Eslint 🔥+ Prettier
+        </p>
+
+        <div className="body">
+          <button onClick={() => setCount((count) => count + 1)}>
+            🪂 Click me : {count}
+          </button>
+
+          <p> Don&apos;t forgot to install Eslint and Prettier in Your Vscode.</p>
+
+          <p>
+            Mess up the code in <code>App.tsx </code> and save the file.
+          </p>
+          <p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer">
+              Learn React
+            </a>
+            {' | '}
+            <a
+              className="App-link"
+              href="https://vitejs.dev/guide/features.html"
+              target="_blank"
+              rel="noopener noreferrer">
+              Vite Docs
+            </a>
+          </p>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
